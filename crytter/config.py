de745d62050396ddef9600e2e9ba0411a8b9eb98
BuildTools <unconfigured@null.spigotmc.org>
2021-04-08 14:17:41 -0700

    Added form zero rep rating notice and added config vars

--- crytter/config.py
+++ crytter/config.py
@@ -4,6 +4,8 @@ import os
 class Config:
 	SECRET_KEY = os.environ.get('flask-secret-key')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('flask-sqlalchemy-database-uri')
+	SERVER_NAME = 'crytter.org'
+	PREFERRED_URL_SCHEME = 'https'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
