c7534ea2f029f7a68abe458f88ee12c2e27e8d36
BuildTools <unconfigured@null.spigotmc.org>
2021-04-08 21:44:04 -0700

    Live rates updated

--- crytter/static/utils.js
+++ crytter/static/utils.js
@@ -26,7 +26,7 @@ async function dealValue(hasC, wantsC){
   const usdOfHas = await CBrate(hasC, "USD");
   const usdOfWants = await CBrate(wantsC, "USD");
 
-  return Math.round((usdOfHas / usdOfWants) * 1000 + Number.EPSILON ) / 1000;
+  return Math.round((usdOfHas / usdOfWants) * 100 + Number.EPSILON ) / 100;
 }
 
 async function CBrate(ofCurr, toCurr){
@@ -34,4 +34,27 @@ async function CBrate(ofCurr, toCurr){
   rates = await $.get(url, function(data, status){return data.data.rates;});
   console.log(rates.data.rates[toCurr]);
   return rates.data.rates[toCurr];
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
