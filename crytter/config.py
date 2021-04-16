1af0c1e7dc2642e2570b0bd21c6fc4e69da12ef3
Cudos <vala.bahrami@gmail.com>
2021-04-16 16:11:05 -0700

    Heroku deploy changes

--- crytter/config.py
+++ crytter/config.py
@@ -2,16 +2,16 @@ import os
 
 
 class Config:
-	SECRET_KEY = os.environ.get('flask-secret-key')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('flask-sqlalchemy-database-uri')
+	SECRET_KEY = os.environ.get('crytter-secret-key')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 	# SERVER_NAME = 'crytter.org'
-	RECAPTCHA_PUBLIC_KEY = '6LftYaYaAAAAAOifwTxOZxkBEg_ZGtQBBoRcoyos'
-	RECAPTCHA_PRIVATE_KEY = '6LftYaYaAAAAAGs01PWgb0-sbjrKkhGDXVQi2cKF'
+	RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA-PUBLIC-KEY')
+	RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA-SECRET-KEY')
 	RECAPTCHA_DATA_ATTRS = {'theme': 'dark'}
 	PREFERRED_URL_SCHEME = 'https'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('flask-mail-user')
-	MAIL_PASSWORD = os.environ.get('flask-mail-password')
+	MAIL_USERNAME = os.environ.get('crytter-mail-user')
+	MAIL_PASSWORD = os.environ.get('crytter-mail-password')
