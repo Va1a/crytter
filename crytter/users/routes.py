eec4ff4df30e6f2448781a278396837fcd90d6ea
Cudos <vala.bahrami@gmail.com>
2021-04-20 19:53:26 -0700

    500 error on rating fix

--- crytter/users/routes.py
+++ crytter/users/routes.py
@@ -152,7 +152,7 @@ def ratings(username):
 		else:
 			newRating = Rating(ratee=user, rater=current_user, impact=int(ratePower)*-1, content=form.experience.data)
 			user.rating -= ratePower
-		alert = Alert(assoc_user=user, type='rating', from_user=current_user.id, post_id='')
+		alert = Alert(assoc_user=user, type='rating', from_user=current_user.id, post_id=0)
 		db.session.add(newRating)
 		db.session.add(alert)
 		user.rateBar = rateToHTML(user.rating)
