8ac4bea9b69e85d9c0cd231cf7327df6872a4dac
cudos <n/a>
2021-10-27 23:28:09 -0700

    Go back to production variables

--- crytter/config.py
+++ crytter/config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-	SECRET_KEY = '0x0x0x0x0x'#os.environ.get('crytter-secret-key')
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'#os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
-	RECAPTCHA_PUBLIC_KEY = '6LftYaYaAAAAAOifwTxOZxkBEg_ZGtQBBoRcoyos'#os.environ.get('RECAPTCHA-PUBLIC-KEY')
-	RECAPTCHA_PRIVATE_KEY = '6LftYaYaAAAAAGs01PWgb0-sbjrKkhGDXVQi2cKF'#os.environ.get('RECAPTCHA-SECRET-KEY')
+	SECRET_KEY = os.environ.get('crytter-secret-key')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+	RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA-PUBLIC-KEY')
+	RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA-SECRET-KEY')
 	RECAPTCHA_DATA_ATTRS = {'theme': 'dark'}
 	PREFERRED_URL_SCHEME = 'https'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
